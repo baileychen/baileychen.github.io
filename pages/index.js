0e396c3a393d285422da12de19544f406701d1c3
Bailey Chen <bailey.chen@veeva.com>
2023-03-08 22:50:41 -0800

    remove process.env.VERCEL_ASSETS

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
             >
               By{" "}
               <Image
-                src={`${process.env.VERCEL_ASSETS}/vercel.svg`}
+                src="vercel.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
                 width={100}
@@ -42,7 +42,7 @@ export default function Home() {
         <div className={styles.center}>
           <Image
             className={styles.logo}
-            src={`${process.env.VERCEL_ASSETS}/next.svg`}
+            src="next.svg"
             alt="Next.js Logo"
             width={180}
             height={37}
@@ -50,7 +50,7 @@ export default function Home() {
           />
           <div className={styles.thirteen}>
             <Image
-              src={`${process.env.VERCEL_ASSETS}/thirteen.svg`}
+              src="thirteen.svg"
               alt="13"
               width={40}
               height={31}
